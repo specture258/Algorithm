14950c5ec910a1652c38fdfb2899cd469bc93a68
jimin Hwnag <154061551+specture258@users.noreply.github.com>
2024-12-29 17:52:10 +0900

    [Silver III] Title: 교수가 된 현우, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int t,n;
+int two;
+int five;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> t;
+    
+    while(t--){
+    	cin >> n;
+    	two =0;
+    	five =0;
+    	for(int i=2 ; i<=n; i*=2){
+    		two += n / i;
+		}
+		for(int i=5 ; i<=n; i*=5){
+    		five += n / i;
+		}
+		
+		cout << min(two, five);
+		cout << "\n";
+	}
+    
+
+       
+   
+}
