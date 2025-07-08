520aaa11846b9665b72a8edd05a60a699f175201
jimin Hwnag <154061551+specture258@users.noreply.github.com>
2025-07-08 18:01:40 +0900

    [Gold V] Title: 선 긋기, Time: 364 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+int n;
+int x,y;
+vector<pair<int, int>> v;
+
+int main(){
+   
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   
+   
+   cin >> n;
+   
+   while(n--){
+   	   cin >> x >> y;
+   	   v.push_back({x,y});
+   }
+   
+   sort(v.begin(), v.end());
+   
+   
+   int st = v[0].first;
+   int ed = v[0].second;
+   int res =0;
+   for(int i=1 ; i<v.size(); i++){
+   	    if(v[i].first <= ed){
+   	    	if(v[i].second >= ed){
+   	    		ed = v[i].second;
+			}
+		}
+		else{
+			res += (ed-st);
+			
+			st = v[i].first;
+			ed = v[i].second;
+		}
+   }
+   
+   res += ed-st;
+   cout << res;
+   
+  
+	
+}
